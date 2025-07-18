7356db78628adaed40fc9d4e976b41294164d240
Duskofff <chalainethan@gmail.com>
2025-07-18 11:04:14 +0200

    Update steam_login.php

--- auth/steam_login.php
+++ auth/steam_login.php
@@ -4,6 +4,7 @@ session_start();
 define('STEAM_API_KEY', 'AC5D04FA890C7B417618838B8034C312');
 define('STEAM_LOGIN_URL', 'https://steamcommunity.com/openid/login');
 
+// URL de retour après authentification Steam
 $return_url = 'http://new-dawn.byethost8.com/auth/steam_callback.php';
 
 function getSteamLoginUrl($return_url) {
@@ -19,12 +20,14 @@ function getSteamLoginUrl($return_url) {
     return STEAM_LOGIN_URL . '?' . http_build_query($params);
 }
 
-if (!isset($_SESSION['steam_id'])) {
-    $steam_login_url = getSteamLoginUrl($return_url);
-    header('Location: ' . $steam_login_url);
-    exit();
-} else {
+// Vérifier si l'utilisateur est déjà connecté
+if (isset($_SESSION['steam_id'])) {
     header('Location: ../index.html');
     exit();
 }
+
+// Rediriger vers Steam pour l'authentification
+$steam_login_url = getSteamLoginUrl($return_url);
+header('Location: ' . $steam_login_url);
+exit();
 ?>
